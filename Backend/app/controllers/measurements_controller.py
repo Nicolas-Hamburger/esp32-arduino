0066d49040a0d3de96e511b7254ccf01e4906b79
Omar <omartobon2002@gmail.com>
2024-09-27 16:05:57 -0500

    ac

--- Backend/app/controllers/measurements_controller.py
+++ Backend/app/controllers/measurements_controller.py
@@ -4,36 +4,22 @@ from config.db_config import get_db_connection
 from models.measurements_model import measurements
 from fastapi.encoders import jsonable_encoder
 
-
 class MeasurementController:
 
-    async def get_all_measurements(self):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
-            cursor.execute("""
-                SELECT * FROM measurements
-            """)
-            result = cursor.fetchall()
-            conn.close()
-            return result
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=str(error))
+    async def post_measurement(self, new_measurement: measurements):
 
-    async def get_measurement_by_id(self, measurement_id: int):
         try:
             conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
+            cursor = conn.cursor()
+            value_motor = new_measurement.value_motor
+            value_led =new_measurement.value_led
             cursor.execute("""
-                SELECT * FROM measurements
-                WHERE measurement_id = %s
-            """, (measurement_id,))
-            result = cursor.fetchone()
+                INSERT INTO measurements (value_motor, value_led)
+                VALUES (%s, %s)
+            """, (value_motor, value_led))
+            conn.commit()
             conn.close()
-            if result:
-                return result
-            else:
-                return {"detail": "Medición no encontrada"}
+            return {"informacion": "Medición registrada"}
         except Exception as error:
             raise HTTPException(status_code=500, detail=str(error))
 
@@ -56,59 +42,6 @@ class MeasurementController:
         except Exception as error:
             raise HTTPException(status_code=500, detail=str(error))
 
-    async def post_measurement(self, new_measurement: measurements):
-
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            value_motor = new_measurement.value_motor
-            value_led = new_measurement.value_led
-            cursor.execute("""
-                INSERT INTO measurements (value_motor, value_led)
-                VALUES (%s, %s)
-            """, (value_motor, value_led))
-            conn.commit()
-            conn.close()
-            return {"informacion": "Medición registrada"}
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=str(error))
-
-    async def update_measurement(self, measurement_id: int, updated_measurement: measurements):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            value_motor = updated_measurement.value_motor
-            value_led = updated_measurement.value_led
-            cursor.execute("""
-            UPDATE measurements
-            SET value_motor = %s, value_led = %s
-            WHERE measurement_id = %s
-        """, (value_motor, value_led, measurement_id))
-            conn.commit()
-            conn.close()
-            return {"informacion": "Medición actualizada"}
-        except Exception as error:
-            raise HTTPException(status_code=500, detail=str(error))
-
-    def delete_measurement(self, measurement_id: int):
-        try:
-            conn = get_db_connection()
-            cursor = conn.cursor()
-            cursor.execute(
-                "SELECT measurement_id FROM measurement WHERE measurement_id = %s", (measurement_id,))
-            resultado = cursor.fetchone()
-            if not resultado:
-                return {"informacion": "El movimiento no se encuentra en la base de datos"}
-            cursor.execute(
-                "DELETE FROM measurement WHERE measurement_id = %s", (measurement_id,))
-            conn.commit()
-            cursor.close()
-            cursor = conn.cursor()
-            cursor.execute("ALTER TABLE measurement AUTO_INCREMENT = 1")
-            conn.commit()
-            return {"informacion": "movimiento eliminado"}
-        except Exception as error:
-            return {"resultado": str(error)}
 
 
 measurement_controller = MeasurementController()
--- Backend/app/routes/measurements_routes.py
+++ Backend/app/routes/measurements_routes.py
@@ -6,36 +6,12 @@ router = APIRouter()
 controller = MeasurementController()
 
 
-@router.get("/get/measurements")
-async def get_all_measurements():
-    return await controller.get_all_measurements()
-
-
-@router.get("/get/measurements/{measurement_id}")
-async def get_measurement_by_id(measurement_id: int):
-    return await controller.get_measurement_by_id(measurement_id)
-
-
-@router.get("/get/measurements/latest")
-async def get_latest_measurements():
-    return await controller.get_latest_measurements()
-
-
 @router.post("/post/measurements")
 async def post_measurement(new_measurement: measurements):
     rpta = await controller.post_measurement(new_measurement)
     return rpta
 
 
-@router.put("/update/measurements/{measurement_id}")
-async def update_measurement(measurement_id: int, updated_measurement: measurements):
-    return await controller.update_measurement(measurement_id, updated_measurement)
-
-
-@router.delete("/delete/measurement/{measurement_id}")
-async def delete_measurement(measurement_id: int):
-    try:
-        rpta = controller.delete_measurement(measurement_id)
-        return rpta
-    except Exception as error:
-        raise HTTPException(status_code=500, detail=str(error))
+@router.get("/get/measurements/latest")
+async def get_latest_measurements():
+    return await controller.get_latest_measurements()
