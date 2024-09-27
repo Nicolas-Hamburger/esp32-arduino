257796152456c41fff947a6ca59d4c219e8e9950
Omar <omartobon2002@gmail.com>
2024-09-27 15:56:32 -0500

    Actualizacion

--- Backend/app/models/sessions_model.py
+++ Backend/app/models/sessions_model.py
@@ -2,7 +2,4 @@ from pydantic import BaseModel
 
 
 class sessions (BaseModel):
-
-    session_id: int
     user_id: int
-    start_time: int
--- Backend/app/models/users_model.py
+++ Backend/app/models/users_model.py
@@ -3,8 +3,6 @@ from datetime import datetime
 
 
 class users (BaseModel):
-    user_id:int
     user_name: str
     email: str
     password: str
-    time_register: datetime
