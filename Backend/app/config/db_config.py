7d3861aa743f15235531afc87359e4f8e02abf2b
Omar <omartobon2002@gmail.com>
2024-10-18 13:59:25 -0500

    ac

--- Backend/app/config/db_config.py
+++ Backend/app/config/db_config.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host= "bbzs5uhupardgtjsrj2e-mysql.services.clever-cloud.com",
-        user="unqepeoln1jfpyhn",
-        password="xMaEL61aZuYjz0iL9CVw",
-        database="bbzs5uhupardgtjsrj2e"
+        host= "localhost",
+        user="root",
+        password="",
+        database="circuit-system"
     )
 
